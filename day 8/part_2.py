ff74e5622eafe3a2acfb055eac35fabf9f3622a1
hadrian-reppas <vreppas22@andover.edu>
2021-12-09 00:19:48 -0500

    Add day 9

--- day 8/part_2.py
+++ day 8/part_2.py
@@ -5,21 +5,26 @@ num_list = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 
 nums = {frozenset(num_list[n]): n for n in range(10)}
 
 def get_mapping(left):
-    for candidate_mapping in map(make_mapping, permutations(letters)):
+    for candidate_mapping in mappings():
         if valid_mapping(candidate_mapping, left):
             return candidate_mapping
 
-make_mapping = lambda vals: {c: v for c, v in zip(letters, vals)}
+def mappings():
+    for perm in permutations(letters):
+        yield {c: p for c, p in zip(letters, perm)}
+
 valid_mapping = lambda mapping, left: all(is_num(mapping, x) for x in left)
 is_num = lambda mapping, x: frozenset(mapping[c] for c in x) in nums
 get_val = lambda mapping, right: int(''.join(str(apply_mapping(mapping, x)) for x in right))
 apply_mapping = lambda mapping, x: nums[frozenset(mapping[c] for c in x)]
 
-def decode(left, right):
+def decode(line):
+    left, right = line
     mapping = get_mapping(left)
     return get_val(mapping, right)
 
 inp = ''.join(open('input.txt'))
-lines = [(line.split(' | ')[0].split(' '), line.split(' | ')[1].split(' ')) for line in inp.split('\n')]
+lines = [tuple(x.split(' ') for x in line.split(' | ')) for line in inp.split('\n')]
 
-print(sum(decode(left, right) for left, right in lines))
+total = sum(decode(line) for line in lines)
+print(total)
