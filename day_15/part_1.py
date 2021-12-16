b0728a14e1c7baab864b53f3fe03f6d70bd3ea8e
hadrian-reppas <vreppas22@andover.edu>
2021-12-16 10:58:11 -0500

    Remove networkx dependency

--- day_15/part_1.py
+++ day_15/part_1.py
@@ -1,17 +1,44 @@
 inp = ''.join(open('input.txt'))
 nums = [[int(n) for n in line] for line in inp.split('\n')]
 
+def add_edge(G, u, v, w, k):
+    for _ in range(w - 1):
+        G[u].add(k)
+        G[k] = set()
+        u = k
+        k += 1
+    G[u].add(v)
+    return k
+
 rows, cols = len(nums), len(nums[0])
 
-nums[0][0] = 0
+G = {(i, j): set() for i in range(rows) for j in range(cols)}
 
-for i in range(rows - 2, -1, -1):
-    nums[i][-1] += nums[i + 1][-1]
-for j in range(cols - 2, -1, -1):
-    nums[-1][j] += nums[-1][j + 1]
+k = 0
+for i in range(rows):
+    for j in range(cols):
+        if i > 0:
+            k = add_edge(G, (i, j), (i - 1, j), nums[i - 1][j], k)
+        if j > 0:
+            k = add_edge(G, (i, j), (i, j - 1), nums[i][j - 1], k)
+        if i < rows - 1: # i < 5*rows - 1:
+            k = add_edge(G, (i, j), (i + 1, j), nums[i + 1][j], k)
+        if j < cols - 1: # j < 5*cols - 1:
+            k = add_edge(G, (i, j), (i, j + 1), nums[i][j + 1], k)
 
-for i in range(rows - 2, -1, -1):
-    for j in range(cols - 2, -1, -1):
-        nums[i][j] += min(nums[i + 1][j], nums[i][j + 1])
+def bfs(G, start, end):
+    queue = [(start, 0)]
+    visited = {start}
+    while queue:
+        v, w = queue.pop(0)
+        for n in G[v]:
+            if n in visited:
+                continue
+            if n == end:
+                return w + 1
+            visited.add(n)
+            queue.append((n, w + 1))
+    assert False
 
-print(nums[0][0])
+dist = bfs(G, (0, 0), (rows - 1, cols - 1))
+print(dist)
--- day_15/part_2.py
+++ day_15/part_2.py
@@ -1,26 +1,49 @@
-import networkx as nx
-
 inp = ''.join(open('input.txt'))
 nums = [[int(n) for n in line] for line in inp.split('\n')]
-
-rows, cols = len(nums), len(nums[0])
 
 def get(i, j):
     a, b = i//rows, j//cols
     m, n = i%rows, j%cols
     return (nums[m][n] - 1 + a + b)%9 + 1
 
-G = nx.DiGraph()
+def add_edge(G, u, v, w, k):
+    for _ in range(w - 1):
+        G[u].add(k)
+        G[k] = set()
+        u = k
+        k += 1
+    G[u].add(v)
+    return k
+
+rows, cols = len(nums), len(nums[0])
+
+G = {(i, j): set() for i in range(5*rows) for j in range(5*cols)}
+
+k = 0
 for i in range(5*rows):
     for j in range(5*cols):
         if i > 0:
-            G.add_edge((i, j), (i - 1, j), weight=get(i - 1, j))
+            k = add_edge(G, (i, j), (i - 1, j), get(i - 1, j), k)
         if j > 0:
-            G.add_edge((i, j), (i, j - 1), weight=get(i, j - 1))
+            k = add_edge(G, (i, j), (i, j - 1), get(i, j - 1), k)
         if i < 5*rows - 1:
-            G.add_edge((i, j), (i + 1, j), weight=get(i + 1, j))
+            k = add_edge(G, (i, j), (i + 1, j), get(i + 1, j), k)
         if j < 5*cols - 1:
-            G.add_edge((i, j), (i, j + 1), weight=get(i, j + 1))
+            k = add_edge(G, (i, j), (i, j + 1), get(i, j + 1), k)
 
-length = nx.algorithms.shortest_paths.astar.astar_path_length(G, (0, 0), (5*rows - 1, 5*cols - 1))
-print(length)
+def bfs(G, start, end):
+    queue = [(start, 0)]
+    visited = {start}
+    while queue:
+        v, w = queue.pop(0)
+        for n in G[v]:
+            if n in visited:
+                continue
+            if n == end:
+                return w + 1
+            visited.add(n)
+            queue.append((n, w + 1))
+    assert False
+
+dist = bfs(G, (0, 0), (5*rows - 1, 5*cols - 1))
+print(dist)
